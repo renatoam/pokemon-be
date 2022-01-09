b25e19e81b3fc8a8e27555a32db5cd3e2966e0f6
˜Renato˜ <renato.melo@ab-inbev.com>
2022-01-09 20:22:29 -0300

    feat: update get all pokemons service

--- src/controllers/GetAllPokemonsController.ts
+++ src/controllers/GetAllPokemonsController.ts
@@ -5,7 +5,7 @@ import { GetAllPokemonsService } from '../services/GetAllPokemonsService';
 export class GetAllPokemonsController {
   async handle(request: Request, response: Response) {
     const { limit } = request.query;
-    const take = Number(limit) || 0;
+    const take = Number(limit) || 10;
 
     const service = new GetAllPokemonsService();
     const pokemons = await service.execute(take);
