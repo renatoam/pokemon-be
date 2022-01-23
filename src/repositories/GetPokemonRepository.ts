025aee52b65afdfe390aca60cae471c68488cbce
˜Renato˜ <renato.melo@ab-inbev.com>
2022-01-23 12:19:35 -0300

    refactor: update prisma schema and depending services

--- src/repositories/GetPokemonRepository.ts
+++ src/repositories/GetPokemonRepository.ts
@@ -3,6 +3,10 @@ import prisma from "../prisma";
 
 export class GetPokemonRepository {
   async findbyId(id: number): Promise<PokemonModel | null> {
+    const test = await prisma.pokemon_stats.findMany()
+
+    console.log({ test })
+
     return await prisma.pokemon.findUnique({
       where: {
         id
--- src/repositories/GetTrainerRepository.ts
+++ src/repositories/GetTrainerRepository.ts
@@ -1,5 +1,7 @@
 import prisma from "../prisma";
 
+// ALTERAR NO BANCO TODOS OS IDS PRA UUID
+
 export class GetTrainerRepository {
   async findById(id: string) {
     return await prisma.trainer.findUnique({
